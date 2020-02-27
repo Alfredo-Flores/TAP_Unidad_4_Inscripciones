84c2c45667fe49353ae915daf4b565ce303cd5a5
Alfredo Flores Garcia <alfredo.f.g.156@gmail.com>
2020-02-26 23:14:19 -0600

    Terminado

--- src/Model/Cliente.java
+++ src/Model/Cliente.java
@@ -1,6 +1,8 @@
 package Model;
 
-public class Cliente {
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
 
     private int id;
     private String nombre;
@@ -8,9 +10,71 @@ public class Cliente {
     private String apellidoMaterno;
     private String telefono;
     private String direccion;
-    private int salario;
+    private boolean deleted;
 
+    public Cliente(int id, String nombre, String apellidoPaterno, String apellidoMaterno, String telefono, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.deleted = false;
+    }
 
-    public Cliente() {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
     }
 }
--- src/Views/Login.java
+++ src/Views/Login.java
@@ -1,5 +1,15 @@
 package Views;
 
+/*
+
+    Hecho por Alfredo Flores Garcia
+    26 de febrero del 2020
+    Clinica Dental
+
+*/
+
+import Model.Usuario;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,56 +20,49 @@ import java.util.Map;
 
 public class Login extends JFrame implements ActionListener {
 
-    JFrame recepcionistaview;
+    JFrame dashboard;
 
     JPanel LoginPanel = new JPanel();
-
-    JLabel LabelTitulo = new JLabel("OdontoClinic");
-
-    ImageIcon image = new ImageIcon(new ImageIcon("src/Views/assets/login_welcome.jpg").getImage().getScaledInstance(575, 480, Image.SCALE_DEFAULT));
+    ImageIcon image = new ImageIcon(new ImageIcon("src/Views/assets/login_welcome.jpg").getImage().getScaledInstance(575, 480, Image.SCALE_SMOOTH));
+    ImageIcon imagebrand = new ImageIcon(new ImageIcon("src/Views/assets/brand.png").getImage().getScaledInstance(220, 75, Image.SCALE_SMOOTH));
+    Image imagetooth = new ImageIcon("src/Views/assets/logo.png").getImage();
+    JLabel LabelTitulo = new JLabel(imagebrand);
     JLabel LabelImagen = new JLabel(image);
-
     JLabel LabelUser = new JLabel("Usuario: ");
     JLabel LabelPassword = new JLabel("Contraseña: ");
-
     JTextField FieldUser = new JTextField(10);
     JPasswordField FieldPassword = new JPasswordField(10);
-
     JButton ButtonAttemp = new JButton("Iniciar Sesión");
+    File montserratregular = new File("src/Views/assets/Montserrat-Regular.ttf");
+    File montserratbold = new File("src/Views/assets/Montserrat-Bold.ttf");
+    Font fontregular = Font.createFont(Font.TRUETYPE_FONT, montserratregular);
+    Font fontbold = Font.createFont(Font.TRUETYPE_FONT, montserratbold);
 
-    Login() {
+    Login() throws IOException, FontFormatException {
         super("Iniciar sesión");
         setSize(850, 480);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
-
+        setIconImage(imagetooth);
+        Font sizedFont = fontbold.deriveFont(16f);
+        Font sizedFontRegular = fontregular.deriveFont(14f);
         ButtonAttemp.addActionListener(this);
-
         LoginPanel.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-
-        Insets inset = new Insets(50, 10, 20, 10);
-
-        String fontname = LabelTitulo.getFont().getName();
-        LabelTitulo.setFont(new Font(fontname, Font.BOLD, 20));
-
+        Insets inset = new Insets(50, 10, 0, 10);
         c.gridx = 0;
         c.gridy = 0;
         c.insets = inset;
         LoginPanel.add(LabelTitulo, c);
-
         inset = new Insets(0, 0, 0, 0);
-
         c.gridx = 1;
         c.gridy = 0;
         c.gridheight = 6;
         c.insets = inset;
         c.fill = GridBagConstraints.BOTH;
         LoginPanel.add(LabelImagen, c);
-
-        inset = new Insets(180, 10, 5, 10);
-
+        inset = new Insets(120, 30, 5, 10);
         c.gridx = 0;
         c.gridy = 1;
         c.gridheight = 1;
@@ -67,94 +70,108 @@ public class Login extends JFrame implements ActionListener {
         c.fill = GridBagConstraints.NONE;
         c.weightx = 0.1;
         c.weighty = 0.1;
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
+        LabelUser.setHorizontalAlignment(JLabel.CENTER);
+        LabelUser.setFont(sizedFontRegular);
         LoginPanel.add(LabelUser, c);
-
         inset = new Insets(5, 30, 5, 30);
         c.gridx = 0;
         c.gridy = 2;
         c.insets = inset;
         c.fill = GridBagConstraints.HORIZONTAL;
+        FieldUser.setFont(sizedFontRegular);
         LoginPanel.add(FieldUser, c);
-
         c.gridx = 0;
         c.gridy = 3;
         c.insets = inset;
         c.fill = GridBagConstraints.NONE;
+        LabelPassword.setHorizontalAlignment(JLabel.CENTER);
+        LabelPassword.setFont(sizedFontRegular);
         LoginPanel.add(LabelPassword, c);
-
         c.gridx = 0;
         c.gridy = 4;
         c.insets = inset;
         c.fill = GridBagConstraints.HORIZONTAL;
+        FieldPassword.setFont(sizedFont);
         LoginPanel.add(FieldPassword, c);
 
         FieldPassword.addActionListener(this);
 
-        inset = new Insets(5, 30, 50, 30);
+        inset = new Insets(20, 30, 50, 30);
 
         c.insets = inset;
         c.gridx = 0;
         c.gridy = 5;
         c.fill = GridBagConstraints.NONE;
+        c.anchor = GridBagConstraints.CENTER;
+        ButtonAttemp.setFont(sizedFont);
         LoginPanel.add(ButtonAttemp, c);
 
         setContentPane(LoginPanel);
     }
-
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         Object Objecto = actionEvent.getSource();
 
         if (Objecto == ButtonAttemp) {
-            this.login();
+            try {
+                this.login();
+            } catch (IOException | FontFormatException e) {
+                e.printStackTrace();
+            }
         } if (Objecto == FieldPassword) {
-            this.login();
+            try {
+                this.login();
+            } catch (IOException | FontFormatException e) {
+                e.printStackTrace();
+            }
         }
+
         repaint();
     }
 
-    private void login() {
-        try {
+    private void login() throws IOException, FontFormatException {
 
-            boolean response = this.attempLogin();
+            int response = this.attempLogin();
 
-            if (response) {
-                // Borrar Views.Login Frame
+            if (response == 1) {
                 this.dispose();
-
-                // Abrir Views.Dashboard Frame
-                recepcionistaview = new RecepcionistaView();
-                recepcionistaview.setVisible(true);
+                dashboard = new RecepcionistaView();
+            } else if (response == 2) {
+                this.dispose();
+                dashboard = new DuenoView();
             } else {
                 JOptionPane.showMessageDialog(LoginPanel, "Estas credenciales no existen, intente de nuevo");
             }
 
-        } catch (Exception ignored) {
-        }
     }
 
-
-    private void createUser(String username, String password) {
+    private void createRecepcionsita(String username, String password) {
 
         // Info Usuario
-        String[] user = {username, password};
+        Usuario nuevousuario;
 
-        HashMap<Integer, String[]> users = null;
+        HashMap<Integer, Usuario> usuarios = null;
 
         try {
             // En caso de que haya un archivo existente con usuario
-            FileInputStream fis = new FileInputStream("usuarios.obj");
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            users = (HashMap) ois.readObject();
-            ois.close();
-            fis.close();
+            FileInputStream filestream = new FileInputStream("usuarios.obj");
+            ObjectInputStream objectstream = new ObjectInputStream(filestream);
+            usuarios = (HashMap) objectstream.readObject();
+            objectstream.close();
+            filestream.close();
 
-            users.put(users.size() + 2, user);
+            int id = usuarios.size() + 2;
+
+            nuevousuario = new Usuario(id, username, password, 0);
+
+            usuarios.put(id, nuevousuario);
         } catch (Exception e) {
             // En caso de que no exista archivo
-            users = new HashMap<Integer, String[]>();
-            users.put(0, user);
+            usuarios = new HashMap<Integer, Usuario>();
+            nuevousuario = new Usuario(1, username, password, 0);
+            usuarios.put(1, nuevousuario);
         }
 
         // Sobreescribir / Escribir archivo
@@ -162,7 +179,9 @@ public class Login extends JFrame implements ActionListener {
             File file = new File("usuarios.obj");
             FileOutputStream fileStream = new FileOutputStream(file);
             ObjectOutputStream objectStream = new ObjectOutputStream(fileStream);
-            objectStream.writeObject(users);
+
+            objectStream.writeObject(usuarios);
+
             fileStream.close();
             objectStream.close();
         } catch (Exception e) {
@@ -171,52 +190,91 @@ public class Login extends JFrame implements ActionListener {
         }
     }
 
-    private boolean attempLogin() {
+    private void createDueno(String username, String password) {
+
+        // Info Usuario
+        Usuario nuevousuario;
+
+        HashMap<Integer, Usuario> usuarios = null;
+
+        try {
+            // En caso de que haya un archivo existente con usuario
+            FileInputStream filestream = new FileInputStream("usuarios.obj");
+            ObjectInputStream objectstream = new ObjectInputStream(filestream);
+            usuarios = (HashMap) objectstream.readObject();
+            objectstream.close();
+            filestream.close();
+
+            int id = usuarios.size() + 2;
+
+            nuevousuario = new Usuario(id, username, password, 1);
+
+            usuarios.put(id, nuevousuario);
+        } catch (Exception e) {
+            // En caso de que no exista archivo
+            usuarios = new HashMap<Integer, Usuario>();
+            nuevousuario = new Usuario(1, username, password, 1);
+            usuarios.put(0, nuevousuario);
+        }
+
+        // Sobreescribir / Escribir archivo
+        try {
+            File file = new File("usuarios.obj");
+            FileOutputStream fileStream = new FileOutputStream(file);
+            ObjectOutputStream objectStream = new ObjectOutputStream(fileStream);
+
+            objectStream.writeObject(usuarios);
+
+            fileStream.close();
+            objectStream.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(LoginPanel, "Ocurrio un error inesperado 1");
+            return;
+        }
+    }
+
+    private int attempLogin() {
         String username, password;
         char[] password_encrypted;
-
-        // Validación
         try {
             username = this.FieldUser.getText();
         } catch (Exception error) {
             JOptionPane.showMessageDialog(LoginPanel, "Escriba el usuario correctamente");
-            return false;
+            return 0;
         }
-
         try {
             password_encrypted = this.FieldPassword.getPassword();
             password = new String(password_encrypted);
         } catch (Exception error) {
             JOptionPane.showMessageDialog(LoginPanel, "Escriba la contraseña correctamente");
-            return false;
+            return 0;
         }
-
-        // Obtener Usuarios
-        HashMap<Integer, String[]> users;
+        createDueno("root", "root");
+        createRecepcionsita("alfredo", "alfredo");
+        HashMap<Integer, Usuario> usuarios;
         try {
             FileInputStream fis = new FileInputStream("usuarios.obj");
             ObjectInputStream ois = new ObjectInputStream(fis);
-            users = (HashMap) ois.readObject();
+            usuarios = (HashMap) ois.readObject();
             ois.close();
             fis.close();
         } catch (Exception e) {
             JOptionPane.showMessageDialog(LoginPanel, "Ocurrio un error inesperado, no existen usuario registrados");
-            return false;
+            return 0;
         }
-
-        // Intento de login
-        String[] user = {username, password};
-
-        for (Map.Entry<Integer, String[]> entry : users.entrySet()) {
-            if (entry.getValue()[0].equals(user[0]) && entry.getValue()[1].equals(user[1])) {
-                return true;
+        for (Map.Entry<Integer, Usuario> entry : usuarios.entrySet()) {
+            if (entry.getValue().getUsername().equals(username) && entry.getValue().getPassword().equals(password)) {
+                if (entry.getValue().getTipo() == 0) {
+                    return 1;
+                } else {
+                    return 2;
+                }
             }
         }
-
-        return false;
+        return 0;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, FontFormatException {
         JFrame login = new Login();
         login.setVisible(true);
     }
